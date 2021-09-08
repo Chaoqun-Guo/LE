58b46b005bb9cdffa727c204aea37b34d2e7b002
Chaoqun-Guo <chaoqunguo317@outlook.com>
2021-09-08 15:26:52 +0800

    add sqoop import sh

---
+++ atlas/conf/sqoop-import-from-mysql-to-hive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sqoop import --connect jdbc:mysql://mysql:3306/$1 \
+            --username root \
+            --password root \
+            --table $2 \
+            --target-dir /user/hive/warehouse/$3 \
+            --delete-target-dir \
+            --hive-import \
+            --hive-overwrite \
+            --hive-database $4 \
+            --hive-table $5
